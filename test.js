af4f22e6a279b37d8b7f055dd3c66f7cbf81062b
Catherine <catherine.e.taber@gmail.com>
2018-03-03 12:25:04 -0600

    First javascript program.

---
+++ test.js
@@ -0,0 +1,15 @@
+var name = "Catherine";
+var born = 1991;
+var year = 2018;
+var age = 26;
+
+console.log ("My name is " + name);
+console.log ("I am " + age + " years old.");
+
+if (age > 40) {
+	console.log("Damn...you are OLD!");
+} else if (age == 40) {
+	console.log("Soon you will be old as well.");
+} else {
+	console.log("I hate you!");
+}
